536b14eaa34433c25b9f305c3ec3c18465c72202
Dan Prince <dan.prince@rackspace.com>
2011-08-15 10:08:30 -0400

    Use after_initialize to set default opts on Glance builder.

--- app/models/glance_package_builder.rb
+++ app/models/glance_package_builder.rb
@@ -1,2 +1,10 @@
 class GlancePackageBuilder < PackageBuilder
+
+  after_initialize :handle_after_initialize
+
+  def handle_after_initialize
+    self.url = ENV['GLANCE_GIT_MASTER']
+    self.branch = "master"
+  end
+
 end
