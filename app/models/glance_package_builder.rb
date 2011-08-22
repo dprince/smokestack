d2046402b901539d834560ddffa6cc72834d8a83
Dan Prince <dan.prince@rackspace.com>
2011-08-22 12:17:00 -0400

    Only initialize the glance url/branch if it isn't set.

--- app/models/glance_package_builder.rb
+++ app/models/glance_package_builder.rb
@@ -3,8 +3,10 @@ class GlancePackageBuilder < PackageBuilder
   after_initialize :handle_after_initialize
 
   def handle_after_initialize
-    self.url = ENV['GLANCE_GIT_MASTER']
-    self.branch = "master"
+    if not self.url and not self.branch then
+      self.url = ENV['GLANCE_GIT_MASTER']
+      self.branch = "master"
+    end
   end
 
 end
