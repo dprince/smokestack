2ee3a7ca52001769afcb914fdffe4619d7d5f2d4
Dan Prince <dan.prince@rackspace.com>
2011-10-30 08:45:46 -0400

    Update package builders so that merge_trunk is false by default.

--- app/models/package_builder.rb
+++ app/models/package_builder.rb
@@ -3,4 +3,12 @@ class PackageBuilder < ActiveRecord::Base
   validates_presence_of :url
   belongs_to :smoke_test, :polymorphic => true
 
+  after_initialize :handle_after_init
+
+  def handle_after_init
+    if new_record? then
+      self.merge_trunk = false
+    end
+  end
+
 end
