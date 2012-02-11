b15b93b33a477c6dad53189f92b0c8715a3d52b4
Dan Prince <dprince@redhat.com>
2012-02-11 13:28:48 -0500

    Implement a simple git clone retry.

--- app/templates/common.sh
+++ app/templates/common.sh
@@ -29,6 +29,17 @@ function fail {
 	exit 1
 }
 
+function git_clone {
+        local URL=${1:?"Please specify a URL."}
+        local DIR=${2:?"Please specify a DIR."}
+        local COUNT=1
+        until GIT_ASKPASS=echo git clone "$URL" "$DIR"; do
+                [ "$COUNT" -eq "4" ] && fail "Failed to clone: $URL"
+                sleep $(( $COUNT * 5 ))
+                COUNT=$(( $COUNT + 1 ))
+        done
+}
+
 function get_nova_source_bzr {
 
 	bzr branch "$NOVA_URL" nova_source || \
@@ -56,8 +67,7 @@ function get_nova_source_bzr {
 
 function get_nova_source_git {
 
-	GIT_ASKPASS=echo git clone $NOVA_GIT_MASTER nova_source || \
-		fail "Failed to git clone nova."
+	git_clone "$NOVA_GIT_MASTER" "nova_source"
 	pushd nova_source
 	git fetch $NOVA_URL $NOVA_BRANCH || \
 		fail "Failed to git fetch branch $NOVA_BRANCH."
@@ -108,8 +118,7 @@ function get_keystone_source_bzr {
 
 function get_keystone_source_git {
 
-	GIT_ASKPASS=echo git clone $KEYSTONE_GIT_MASTER keystone_source || \
-		fail "Failed to git clone keystone."
+	git_clone "$KEYSTONE_GIT_MASTER" "keystone_source"
 	pushd keystone_source
 	git fetch $KEYSTONE_URL $KEYSTONE_BRANCH || \
 		fail "Failed to git fetch branch $KEYSTONE_BRANCH."
@@ -159,8 +168,7 @@ function get_glance_source_bzr {
 
 function get_glance_source_git {
 
-	GIT_ASKPASS=echo git clone $GLANCE_GIT_MASTER glance_source || \
-		fail "Failed to git clone glance."
+	git_clone "$GLANCE_GIT_MASTER" "glance_source"
 	pushd glance_source
 	git fetch $GLANCE_URL $GLANCE_BRANCH || \
 		fail "Failed to git fetch branch $GLANCE_BRANCH."
