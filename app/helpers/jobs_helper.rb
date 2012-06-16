9e965b45d26b6e4ba60d3491572680759f22147b
Dan Prince <dprince@redhat.com>
2012-06-16 15:39:17 -0400

    Fix job runtime display when its over an hour.

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -14,8 +14,8 @@ module JobsHelper
 
         if f then
             hours = (f/60/60).floor
-            minutes = (f/60).floor
-            seconds = (f - (minutes * 60) - (hours * 60 * 60)).to_i
+            minutes = ((f - (hours * 3600))/60).floor
+            seconds = (f - (minutes * 60) - (hours * 3600)).to_i
             return sprintf("%02d:%02d:%02d", hours, minutes, seconds)
         end
 
