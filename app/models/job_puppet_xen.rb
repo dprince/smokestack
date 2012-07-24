9cea2e6446797a75f30ede82c267cdfc17dc4ce5
Dan Prince <dprince@redhat.com>
2012-07-24 14:40:36 -0400

    Add Puppet XenServer runner script and job.

---
+++ app/models/job_puppet_xen.rb
@@ -0,0 +1,22 @@
+class JobPuppetXen < Job
+
+  @queue=:xen
+
+  def self.perform(id)
+    5.times do 
+      begin
+        job=JobPuppetXen.find(id)
+        JobPuppetXen.run_job(job, "puppet_xen_runner.sh.erb")
+        break
+      rescue ActiveRecord::RecordNotFound
+        sleep 5
+      end
+    end
+  end
+
+  after_create :handle_after_create
+  def handle_after_create
+    AsyncExec.run_job(JobPuppetXen, self.id)
+  end
+
+end
