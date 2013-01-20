4e9590c8e0e37fa0324bfe67b6e262468c101427
Dan Prince <dprince@redhat.com>
2013-01-20 14:26:41 -0500

    Restart monit regardless of stop_workers status.

--- config/deploy/worker.rb
+++ config/deploy/worker.rb
@@ -2,7 +2,7 @@ namespace :worker do
 
   desc "Reload all workers after code update."
   task :reload, :roles => :worker do
-    run "#{current_path}/script/stop_workers && /sbin/service monit restart"
+    run "#{current_path}/script/stop_workers; sleep 5; /sbin/service monit restart"
   end
 
 end
